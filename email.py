e705ce30c86b50e327afa3580eee64b3348dd103
Yogesh238 <76422212+Yogesh238@users.noreply.github.com>
2021-06-30 16:44:41 +0530

    Create email.py

---
+++ email.py
@@ -0,0 +1,19 @@
+import boto3
+from botocore.exceptions import ClientError
+from sys import argv
+import requests
+
+ENV = argv[1]
+url = argv[2]
+
+
+RECIPIENT = "sachin.saini@cloverbaytechnologies.com"
+SENDER = "yogesh.p@cloverbaytechnologies.com"
+AWS_REGION = "us-east-1"
+CHARSET = "UTF-8"
+
+
+# The email body for recipients with non-HTML email clients.
+BODY_TEXT = ("\r\n"
+            )
+'http://100.25.211.184:8080/job/demo/build?token=newjob'
